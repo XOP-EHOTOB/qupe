dd821f68134e37ade13148e374ed16bd2c5ec9e5
XOP-EHOTOB <dima.dimitrev@gmail.com>
2022-11-18 16:38:04 +0300

    1.1.0

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -6,18 +6,16 @@ const {check, validationResult} = require('express-validator')
 
 router.post('/generate',  async (req, res) => {
   try {
-
     const baseUrl = process.env.BASE_URL
     const { from } = req.body
 
-    const existing = await Link.findOne({ from: from.toLowerCase() })
-
+    const existing = await Link.findOne({ from })
     if (existing) return res.json({ link: existing })
     
     let code = shortid()
     const to = baseUrl + '/' + code
 
-    const link = new Link({ code, to, from: from.toLowerCase() })
+    const link = new Link({ code, to, from })
     await link.save()
 
     res.status(201).json({ link })
